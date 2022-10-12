0c4be50ab7b2010cf0bd0649ca0519eeb042438f
Yaroslav Snigur <yaroslavsnigur@Yaroslavs-MacBook-Air.local>
2022-10-12 15:06:10 -0400

    some more components added

---
+++ src/components/Meals/MealsSummary.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Good food</h2>
+      <p>Choose food</p>
+      <p>Its really a good food</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
